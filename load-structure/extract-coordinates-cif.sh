15f516abe338ca9c5216306b826ed814c460846a
Lucas Rassilan Vilanova <lucasrsv1@gmail.com>
2023-11-19 19:14:27 -0300

    Correção para arquivos CIF com mais colunas

--- load-structure/extract-coordinates-cif.sh
+++ load-structure/extract-coordinates-cif.sh
@@ -5,24 +5,24 @@ gzip -dfk "$1"
 decompressed_file=${1%.*}
 
 # Encontra o número da linha onde está o cabeçalho do número do modelo
-model_header_line=`awk '$1 ~ /^_atom_site.pdbx_PDB_model_num/ { print NR }' "$decompressed_file"`
+model_header_line=`awk '$1 ~ /^_atom_site.pdbx_PDB_model_num ?$/ { print NR }' "$decompressed_file"`
 
 # Encontra o número da linha onde está o cabeçalho da flag de posição alternativa
-alt_pos_header_line=`awk '$1 ~ /^_atom_site.label_alt_id/ { print NR }' "$decompressed_file"`
+alt_pos_header_line=`awk '$1 ~ /^_atom_site.label_alt_id ?$/ { print NR }' "$decompressed_file"`
 
 # Encontra o número da linha onde está o cabeçalho da coluna da coordenada X
-x_header_line=`awk '$1 ~ /^_atom_site.Cartn_x/ { print NR }' "$decompressed_file"`
+x_header_line=`awk '$1 ~ /^_atom_site.Cartn_x ?$/ { print NR }' "$decompressed_file"`
 
 # Lendo o arquivo de baixo pra cima a partir da linha onde está o cabeçalho da coordenada X,
 # encontra a linha onde se inicia o loop dentre as 25 linhas anteriores.
 # Subtraindo 1 desse resultado temos o número correto da coluna com a coordenada X dos átomos.
-x_col=$((`head -n $x_header_line "$decompressed_file" | tail -n 25 | tac | awk '$1 ~ /^loop_/ { print NR }' | head -n 1` - 1))
+x_col=$((`head -n $x_header_line "$decompressed_file" | tail -n 40 | tac | awk '$1 ~ /^loop_/ { print NR }' | head -n 1` - 1))
 
 y_col=$(($x_col + 1))
 z_col=$(($x_col + 2))
 
-model_col=$((`head -n $model_header_line "$decompressed_file" | tail -n 25 | tac | awk '$1 ~ /^loop_/ { print NR }' | head -n 1` - 1))
-alt_pos_col=$((`head -n $alt_pos_header_line "$decompressed_file" | tail -n 25 | tac | awk '$1 ~ /^loop_/ { print NR }' | head -n 1` - 1))
+model_col=$((`head -n $model_header_line "$decompressed_file" | tail -n 40 | tac | awk '$1 ~ /^loop_/ { print NR }' | head -n 1` - 1))
+alt_pos_col=$((`head -n $alt_pos_header_line "$decompressed_file" | tail -n 40 | tac | awk '$1 ~ /^loop_/ { print NR }' | head -n 1` - 1))
 
 # Imprime apenas as colunas das coordenadas X, Y e Z apenas para as linhas que possuem átomos
 echo "EXTRACTED COORDINATES"
